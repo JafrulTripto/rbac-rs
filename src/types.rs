f35f433be16caf58df58b59ccb1683a7f21481a4
Jafrul Tripto <jafrultripto@gmail.com>
2025-01-30 22:14:36 +0800

    updated types file

--- src/types.rs
+++ src/types.rs
@@ -4,6 +4,8 @@ use uuid::Uuid;
 #[cfg(feature = "incremental-id")]
 pub type Id = i32;
 
-#[cfg(feature = "uuid-id")]
+#[cfg(all(feature = "uuid-id", not(feature = "incremental-id")))]
 pub type Id = Uuid;
 
+#[cfg(all(feature = "incremental-id", not(feature = "uuid-id")))]
+pub type Id = i32;
