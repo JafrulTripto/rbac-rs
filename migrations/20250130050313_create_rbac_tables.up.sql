f193373ebd8b81bc5ec4f2d113d1a5ba00492a68
Jafrul Tripto <jafrultripto@gmail.com>
2025-01-30 20:12:03 +0800

    Added pgcrypto extention

--- migrations/20250130050313_create_rbac_tables.up.sql
+++ migrations/20250130050313_create_rbac_tables.up.sql
@@ -1,4 +1,5 @@
 -- Add userss table
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT NOT NULL UNIQUE,
